37b4ad8641d5620e821fbfa8727fbc69d637e447
robertborsos <rborsos@student.tgm.ac.at>
2015-11-19 17:04:22 +0100

    Aktuelle Version App
    Quake Watch App

---
+++ QuakeWatch/app/src/main/java/com/example/markus/quakewatch/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.markus.quakewatch;
+
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declaring Your View and Variables
+
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[] = {"AT","EU","World","Search"};
+    int Numboftabs =4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Creating The Toolbar and setting it as the Toolbar for the activity
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("");//Setz titel auf "" damit es sich ni cht mit dem Layout überschneidet
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ QuakeWatch/app/src/main/java/com/example/markus/quakewatch/ViewPagerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.markus.quakewatch;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.text.style.CharacterStyle;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    CharSequence Titles[];
+     int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropri ate values in the class
+    public ViewPagerAdapter(FragmentManager fm,CharSequence[] mtitles  , int mNumbOfTabsumb) {
+        super(fm);
+        this.Titles = mtitles;
+         this.NumbOfTabs = mNumbOfTabsumb;
+
+     }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0) // if the position is 0 we are returning the First tab
+        {
+            RecyclerView1 recyclerView1 = new RecyclerView1();
+            return recyclerView1;
+        }
+        else if(position == 1)            // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+             RecyclerView1 tab2 = new RecyclerView1();
+             return tab2;
+        }
+        else if(position == 2)             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+            RecyclerView1 tab3 = new RecyclerView1();
+            return tab3;
+        }
+        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+            Search tab4 = new Search();
+            return tab4;
+        }
+
+
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+     @Override
+     public int getCount() {
+        return NumbOfTabs;
+    }
+}
